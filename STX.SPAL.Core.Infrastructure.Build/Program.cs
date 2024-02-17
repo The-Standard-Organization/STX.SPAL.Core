0d43814da49345fc88f4403c0ab518eac1b8d7f8
Christo du Toit <christo@dutoit.co.uk>
2024-02-17 17:59:29 +0000

    INFRA: Initial chesk-in

---
+++ STX.SPAL.Core.Infrastructure.Build/Program.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using STX.SPAL.Core.Infrastructure.Build.Services;
+
+namespace STX.SPAL.Core.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
