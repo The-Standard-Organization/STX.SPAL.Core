3f17e837efecd4975c0c96b2fa97ae2c0e7d7082
Raúl LB <rlorenb@gmail.com>
2024-05-22 13:34:21 +0200

    MODELS: Abstraction interfaces

---
+++ STX.SPAL.Abstractions/ISPALProvider.cs
@@ -0,0 +1,11 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace STX.SPAL.Abstractions
+{
+    public interface ISPALProvider
+    {
+        string GetSPALId();
+    }
+}
---
+++ STX.SPAL.Abstractions/Providers/IAbstractionProvider.cs
@@ -0,0 +1,41 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+
+namespace STX.SPAL.Abstractions.Providers
+{
+    public interface IAbstractionProvider<T>
+        where T : ISPALProvider
+    {
+        T GetProvider();
+
+        void UseProvider(string spalId = null);
+
+        void UseProvider(
+            Type concreteProviderType = null,
+            string spalId = null);
+
+        void UseProvider<TConcreteProviderType>(
+            string spalId = null)
+            where TConcreteProviderType : T;
+
+        void Invoke<TResult>(Action<T> spalFunction);
+
+        void InvokeWithProvider<TConcreteProviderType, TResult>(
+            Action<T> spalFunction)
+            where TConcreteProviderType : T;
+
+        TResult Invoke<TResult>(
+            Func<T, TResult> spalFunction);
+
+        ValueTask InvokeAsync<TResult>(
+            Func<T, ValueTask> spalFunction);
+
+        ValueTask<TResult> InvokeAsync<TResult>(
+            Func<T, ValueTask<TResult>> spalFunction);
+
+    }
+}
