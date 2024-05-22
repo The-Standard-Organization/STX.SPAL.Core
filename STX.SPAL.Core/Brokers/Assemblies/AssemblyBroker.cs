221a931d909e4b1aca31acb7fd8a3de10b9820be
Raúl LB <rlorenb@gmail.com>
2024-05-22 19:43:32 +0200

    CORE RUB: Refactor GetApplicationPathsAssemblies

--- STX.SPAL.Core/Brokers/Assemblies/AssemblyBroker.cs
+++ STX.SPAL.Core/Brokers/Assemblies/AssemblyBroker.cs
@@ -10,12 +10,22 @@ namespace STX.SPAL.Core.Brokers.Assemblies
     internal partial class AssemblyBroker : IAssemblyBroker
     {
         private readonly string applicationPath;
+        private readonly string[] applicationPathsAssemblies;
 
         public AssemblyBroker()
         {
             applicationPath =
                 Path.GetDirectoryName(
                     Assembly.GetEntryAssembly().Location);
+
+            applicationPathsAssemblies =
+                Directory.GetFiles(applicationPath, "*.dll");
         }
+
+        public string[] GetApplicationPathsAssemblies() =>
+            applicationPathsAssemblies;
+
+        public Assembly GetAssembly(string assemblyPath) =>
+            Assembly.LoadFrom(assemblyPath);
     }
 }
--- STX.SPAL.Core/Brokers/Assemblies/IAssemblyBroker.cs
+++ STX.SPAL.Core/Brokers/Assemblies/IAssemblyBroker.cs
@@ -2,9 +2,12 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Reflection;
+
 namespace STX.SPAL.Core.Brokers.Assemblies
 {
     internal partial interface IAssemblyBroker
     {
+        Assembly GetAssembly(string fullPath);
     }
 }
