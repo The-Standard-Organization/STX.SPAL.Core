313ab5c0942d76585bac63c2fa88875bf4f02277
Raúl LB <rlorenb@gmail.com>
2024-05-23 20:09:04 +0200

    ShouldThrowExceptionIfInvalidAssemblyPath -> FAIL

---
+++ STX.SPAL.Core.Tests.Unit/Services/Foundations/Assemblies/AssemblyServiceTests.Validations.GetAssembly.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using Moq;
+using STX.SPAL.Core.Models.Services.Foundations.Assemblies.Exceptions;
+
+namespace STX.SPAL.Core.Tests.Unit.Services.Foundations.Assemblies
+{
+    public partial class AssemblyServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ShouldThrowExceptionIfInvalidAssemblyPath(string inputPathAssembly)
+        {
+            // given
+            Assembly randomAssembly = CreateRandomAssembly();
+            Assembly expectedAssembly = randomAssembly;
+            Assembly returnedAssembly = randomAssembly;
+            string randomPathAssembly = GetRandomPathAssembly();
+
+            this.assemblyBroker
+                .Setup(broker =>
+                    broker.GetAssembly(
+                        It.Is<string>(actualPathAssembly =>
+                                actualPathAssembly == inputPathAssembly)));
+
+            // when
+            Func<Assembly> getAssemblyFunction = () =>
+                this.assemblyService.GetAssembly(inputPathAssembly);
+
+            Assert.Throws<InvalidAssemblyPathException>(
+                getAssemblyFunction);
+
+            //then
+            this.assemblyBroker.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ STX.SPAL.Core/Models/Services/Foundations/Assemblies/Exceptions/InvalidAssemblyPathException.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace STX.SPAL.Core.Models.Services.Foundations.Assemblies.Exceptions
+{
+    internal class InvalidAssemblyPathException : Xeption
+    {
+        public InvalidAssemblyPathException(string message) : base(message)
+        { }
+    }
+}
