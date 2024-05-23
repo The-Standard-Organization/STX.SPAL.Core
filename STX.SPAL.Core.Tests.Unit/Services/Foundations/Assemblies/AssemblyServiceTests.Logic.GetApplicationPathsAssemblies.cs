2726cad2345567e4012f51895c84426d0d07e340
Raúl LB <rlorenb@gmail.com>
2024-05-23 08:09:32 +0200

    MINOR FIX: Add fluent assertion package

--- STX.SPAL.Core.Tests.Unit/Services/Foundations/Assemblies/AssemblyServiceTests.Logic.GetApplicationPathsAssemblies.cs
+++ STX.SPAL.Core.Tests.Unit/Services/Foundations/Assemblies/AssemblyServiceTests.Logic.GetApplicationPathsAssemblies.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
 
 namespace STX.SPAL.Core.Tests.Unit.Services.Foundations.Assemblies
@@ -28,10 +29,13 @@ namespace STX.SPAL.Core.Tests.Unit.Services.Foundations.Assemblies
                 .Returns(returnedApplicationPathsAssemblies);
 
             // when
-            string[] actuaApplicationPathsAssemblies =
+            string[] actualApplicationPathsAssemblies =
                this.assemblyService.GetApplicationPathsAssemblies();
 
             //then
+            actualApplicationPathsAssemblies.Should()
+                .BeEquivalentTo(expectedApplicationPathsAssemblies);
+
             this.assemblyBroker.Verify(
                 broker =>
                     broker.GetApplicationPathsAssemblies(),
